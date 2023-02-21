c6bb532f7d89dd9ffde5353867f45a29de8437c8
Dorthy Thielsen <dorthythielsen@gmail.com>
2023-02-21 15:18:34 -0500

    working on graph methods

--- graph.js
+++ graph.js
@@ -2,4 +2,23 @@ class Graph {
     constructor(){
         this.adjacencyList = {}
     }
+
+    // this is an undirected graph
+
+    addVertex(vertex){
+    // write a method which accepts a name of a vertex
+    // it should add a key to the adjacency list with the name of the vertex 
+    // and set its value to be an empty array
+        if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = []
+    }
+
+    addEdge(v1, v2){
+        // drawing connections between two vertices
+        // accepts two vertices
+        // find in the adjacencyList the key of the two vertices
+        // push the key of the vertices to the array
+        // don't worry about handling errors/invalid vertices 
+        this.adjacencyList[v1].push(v2)
+        this.adjacencyList[v2].push(v1)
+    }
 }
